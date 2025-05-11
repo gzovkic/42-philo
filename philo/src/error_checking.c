c2b2c407e96e3deea3fb62875e7a114dcfbd00c9
gzovkic <g.zovkic@web.de>
2025-05-11 12:43:02 +0200

    Improve code readability by adding spaces around operators and after control statements

--- philo/src/error_checking.c
+++ philo/src/error_checking.c
@@ -6,7 +6,7 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 12:01:06 by gzovkic           #+#    #+#             */
-/*   Updated: 2025/05/11 12:40:06 by gzovkic          ###   ########.fr       */
+/*   Updated: 2025/05/11 12:42:42 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 bool	check_nbr_numeric(char *argv[])
 {
 	int	index;
-	int count;
+	int	count;
 
 	index = 1;
-	while(argv[index] != NULL)
+	while (argv[index] != NULL)
 	{
 		count = 0;
-		while(argv[index][count] != '\0')
+		while (argv[index][count] != '\0')
 		{
-			if(argv[index][count] < '0' || argv[index][count] > '9')
+			if (argv[index][count] < '0' || argv[index][count] > '9')
 			{
 				(void)printf("Non numeric argument\n");
 				return (true);
@@ -37,7 +37,7 @@ bool	check_nbr_numeric(char *argv[])
 
 bool	check_nbr_arg(int argc)
 {
-	if(argc < 5 || argc > 6)
+	if (argc < 5 || argc > 6)
 	{
 		(void)printf("Wrong number of arguments\n");
 		return (true);
@@ -47,9 +47,9 @@ bool	check_nbr_arg(int argc)
 
 bool	check_arguments(int argc, char *argv[])
 {
-	if(check_nbr_arg(argc))
-		return(true);
-	if(check_nbr_numeric(argv))
-		return(true);
-	return(false);
+	if (check_nbr_arg(argc))
+		return (true);
+	if (check_nbr_numeric(argv))
+		return (true);
+	return (false);
 }
