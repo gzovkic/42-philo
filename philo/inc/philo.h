c237747a4e4d5178e4be3620b46c3c8aff531c81
gzovkic <g.zovkic@web.de>
2025-05-18 13:02:04 +0200

    Update Makefile and refactor dinner structure; adjust argument handling and modify philosopher struct definitions

--- philo/inc/philo.h
+++ philo/inc/philo.h
@@ -6,7 +6,7 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 11:50:22 by gzovkic           #+#    #+#             */
-/*   Updated: 2025/05/15 13:29:20 by gzovkic          ###   ########.fr       */
+/*   Updated: 2025/05/18 12:20:31 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,61 +18,66 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-struct	s_philo;
+struct s_philo_node;
 
 typedef struct s_dinner
 {
-	struct s_philo	*philos;
-	pthread_mutex_t	*forks;
-	int				number_of_philos;
-	time_t			time_to_die;
-	time_t			time_to_sleep;
-	time_t			time_to_eat;
-	time_t			times_must_eat;
-	int				start_timer_of_sim;
-	int				sim_status;
-}					t_dinner;
-
-typedef struct s_philo
+	pthread_mutex_t		*print_action_mutex;
+	int					number_of_philos;
+	time_t				time_to_die;
+	time_t				time_to_sleep;
+	time_t				time_to_eat;
+	time_t				times_must_eat;
+	int					start_timer_of_sim;
+	int					sim_status;
+}						t_dinner;
+typedef struct s_philo_node
 {
-	t_dinner		*dinner;
-	int				philo_id;
-	int				time_since_last_meal;
-	int				meals_eaten;
-	pthread_t		thread;
-}					t_philo;
+	int					philo_id;
+	int					time_since_last_meal;
+	int					meals_eaten;
+	pthread_t			thread;
+	pthread_mutex_t		fork;
+	struct s_philo_node	*next;
+}						t_philo_node;
+typedef struct s_philo_list
+{
+	t_philo_node		*head;
+	t_philo_node		*tail;
+	ssize_t				size;
+	t_dinner			*dinner;
+}						t_philo_list;
+
 
 typedef enum PHILO_ENUMS
 {
 	TIME_MUST_EAT_NOT_SET = -1,
 	SIM_ACTIV,
 	SIM_INACTIV
-}					t_PHILO;
+}						t_PHILO;
 
 // src/main.c
-void				*routine(void *arg);
+void					*routine(void *arg);
 
 // src/error_checking.c
-bool				check_nbr_numeric(char *argv[]);
-bool				check_nbr_arg(int argc);
-bool				check_minus(char *argv[]);
-bool				check_max_int(char *argv[]);
+bool					check_nbr_numeric(char *argv[]);
+bool					check_nbr_arg(int argc);
+bool					check_minus(char *argv[]);
+bool					check_max_int(char *argv[]);
 
 // stc/error_checking_utils.c
-bool				check_arguments(int argc, char *arvg[]);
-bool				check_statments(char *argv[], int index, int count);
+bool					check_arguments(int argc, char *arvg[]);
+bool					check_statments(char *argv[], int index, int count);
 
 // src/fake_libft.c
-size_t				ft_strlen(const char *str);
-long				ft_atol(const char *str);
-int					ft_atoi(const char *str);
-void				ft_putstr_fd(char *s, int fd);
-void				*ft_calloc(size_t count, size_t size);
+size_t					ft_strlen(const char *str);
+long					ft_atol(const char *str);
+int						ft_atoi(const char *str);
+void					ft_putstr_fd(char *s, int fd);
+void					*ft_calloc(size_t count, size_t size);
 
 // src/create_structs.c
-void				create_dinner(t_dinner *dinner, char *argv[]);
-void				create_mutexes(t_dinner *dinner);
-t_philo				*init_philo_struct(t_dinner *dinner);
-
+void					create_dinner(t_dinner *dinner, char *argv[]);
+void					create_mutexes(t_dinner *dinner);
 // src/utils.c
-long				curr_time(void);
+long					curr_time(void);
--- philo/src/create_structs.c
+++ philo/src/create_structs.c
@@ -6,7 +6,7 @@
 /*   By: gzovkic <gzovkic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 13:34:41 by gzovkic           #+#    #+#             */
-/*   Updated: 2025/05/15 13:30:00 by gzovkic          ###   ########.fr       */
+/*   Updated: 2025/05/18 12:46:33 by gzovkic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,34 @@
 
 void	create_dinner(t_dinner *dinner, char *argv[])
 {
-	dinner->number_of_philos = ft_atoi(argv[2]);
-	dinner->time_to_die = ft_atoi(argv[3]);
-	dinner->time_to_eat = ft_atoi(argv[4]);
-	dinner->time_to_sleep = ft_atoi(argv[5]);
-	dinner->times_must_eat = TIME_MUST_EAT_NOT_SET;
-	if (argv[6] != NULL)
-		dinner->times_must_eat = ft_atoi(argv[6]);
+	dinner->number_of_philos = ft_atoi(argv[1]);
+	dinner->time_to_die = ft_atoi(argv[2]);
+	dinner->time_to_eat = ft_atoi(argv[3]);
+	dinner->time_to_sleep = ft_atoi(argv[4]);
+	if (argv[5] != NULL)
+		dinner->times_must_eat = ft_atoi(argv[5]);
+	else
+		dinner->times_must_eat = TIME_MUST_EAT_NOT_SET;
 	dinner->start_timer_of_sim = curr_time();
 	dinner->sim_status = SIM_ACTIV;
 }
 
-void	create_mutexes(t_dinner *dinner)
-{
-	int	count;
+// void	create_mutexes(t_dinner *dinner)
+// {
+// 	int	count;
 
-	dinner->forks = ft_calloc(dinner->number_of_philos,
-			sizeof(pthread_mutex_t));
-	if (!dinner->forks)
-		ft_putstr_fd("Memory alloction failed\n", 2);
-	count = 0;
-	while (count < dinner->number_of_philos)
-	{
-		pthread_mutex_init(&dinner->forks[count], NULL);
-		count++;
-	}
-}
-
-t_philo	*init_philo_struct(t_dinner *dinner)
-{
-	t_philo	*philos;
-	int		id;
-
-	philos = ft_calloc(sizeof(t_philo), dinner->number_of_philos);
-	if (!philos)
-	{
-		ft_putstr_fd("Memory alloction failed for philos\n", 2);
-		return (NULL);
-	}
-	id = 0;
-	while (id < dinner->number_of_philos)
-	{
-		philos[id].philo_id = id + 1;
-		philos->dinner = dinner;
-		philos->time_since_last_meal = dinner->start_timer_of_sim;
-		philos->meals_eaten = 0;
-		philos->thread = id++;
-	}
-	return (philos);
-}
+// 	dinner->forks = ft_calloc(dinner->number_of_philos,
+// 			sizeof(pthread_mutex_t));
+// 	if (!dinner->forks)
+// 		ft_putstr_fd("Memory alloction failed\n", 2);
+// 	count = 0;
+// 	while (count < dinner->number_of_philos)
+// 	{
+// 		if(pthread_mutex_init(&dinner->forks[count], NULL) == -1)
+// 		{
+// 			ft_putstr_fd("Mutex creation failed for forks\n", 2);
+// 			return ;
+// 		}
+// 		count++;
+// 	}
+// }
